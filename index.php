bd19ad33137132ba3d4a1082e3a7452082730d56
Phil Chow <zhoufei@zhoufeideMacBook-Air.local>
2017-05-29 19:44:31 +0800

    add expo

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+<title>2017第15届中国(武汉)国际汽车服务产业博览会</title>
+<meta name="keywords" content="汽车服务产业,武汉后博会,汽车后市场" />
+<meta name="description" content="2017第15届中国(武汉)国际汽车服务产业将于2017年6月16-18日在武汉国际博览中心隆重举行。2017武汉后博会规模达到23万平方米，创下汽车后市场展览行业新纪录。" />
+<!--[if lte IE 6]>
+<script src="/skins/2014/js/DD_belatedPNG_0.0.8a.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        DD_belatedPNG.fix('div, ul, img, li, input , a');
+    </script>
+<![endif]--> 
+</head>
+
+<body style="margin:0; padding:0;background:url(./home_bg.gif) #0e162b repeat-x;">
+
+	<div style=" width:100%; margin:0; padding:0; background:url(./home_bg.jpg) center top no-repeat; height:810px;">
+    	<div style="width:460px; margin:0 auto; position:relative; top:380px;">
+    		<span style="float:left;"><a href="./caw" style="width:174px; height:244px; display:block;"></a></span>
+            <span style="float:right;"><a href="./expo" style="width:174px; height:244px; display:block;"></a></span>
+        </div>
+        
+        
+    </div>
+</body>
+</html>
